fa1f2ab940a69051e1190542987f6edcd5116358
Alain Armand <aarmand.inbox@gmail.com>
2020-01-15 13:28:01 -0500

    back to seperate directory for RxJs

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         use: [
           {
             loader: require.resolve('babel-loader'),
+            /* cacheDirectory: Default false. When set, the given directory will be used to cache the results of the loader. Future webpack builds will attempt to read from the cache to avoid needing to run the potentially expensive Babel recompilation process on each run. If the value is set to true in options ({cacheDirectory: true}), the loader will use the default cache directory in node_modules/.cache/babel-loader or fallback to the default OS temporary file directory if no node_modules folder could be found in any root directory. */
             options: {
               ...babelConfig,
               cacheDirectory: true,
