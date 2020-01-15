625cd12bb3d2cb21953bb657d57c948e02dd1b81
Alain Armand <aarmand.inbox@gmail.com>
2020-01-15 15:34:22 -0500

    simplified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,10 @@
-const webpack = require('webpack')
-const path = require('path')
-const babelConfig = require('./babel.config')
+const path = require('path');
 
 module.exports = {
+  entry: './src/Demo.bs.js',
   mode: 'development',
-  devtool: 'inline-source-map',
-  entry: __dirname + '/src/Demo.bs',
-  target: 'node',
   output: {
-    path: __dirname + '/build',
-    filename: 'Demo.js',
+    path: path.join(__dirname, "build"),
+    filename: 'index.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        include: [__dirname + '/src'],
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-            /* cacheDirectory: Default false. When set, the given directory will be used to cache the results of the loader. Future webpack builds will attempt to read from the cache to avoid needing to run the potentially expensive Babel recompilation process on each run. If the value is set to true in options ({cacheDirectory: true}), the loader will use the default cache directory in node_modules/.cache/babel-loader or fallback to the default OS temporary file directory if no node_modules folder could be found in any root directory. */
-            options: {
-              ...babelConfig,
-              cacheDirectory: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-}
+};
